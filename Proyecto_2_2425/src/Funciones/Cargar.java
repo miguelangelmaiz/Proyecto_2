2399e502a1d763a3469f8da98d026ed1409b992a
Miguelangel Maiz <miguelangel.maiz@correo.unimet.edu.ve>
2025-03-22 12:39:30 -0400

    Update Cargar.java

--- Proyecto_2_2425/src/Funciones/Cargar.java
+++ Proyecto_2_2425/src/Funciones/Cargar.java
@@ -16,40 +16,6 @@ import org.json.JSONObject;
  * @author Miguel
  */
 public class Cargar {
-    public static void cargarClave(String rutaArchivo) {
-        try {
-            // Leer el contenido del archivo JSON
-            String contenido = new String(Files.readAllBytes(Paths.get(rutaArchivo)));
-            JSONObject jsonObject = new JSONObject(contenido);
-            procesarClaveDicotomica(jsonObject);
-        } catch (Exception e) {
-            System.out.println("Error al leer el archivo JSON: " + e.getMessage());
-        }
-    }
-
-    public static void procesarClaveDicotomica(JSONObject jsonObject) {
-        for (String clave : jsonObject.keySet()) {
-            JSONArray especiesArray = jsonObject.getJSONArray(clave);
-            System.out.println("Clave Dicotómica: " + clave);
-
-            for (int i = 0; i < especiesArray.length(); i++) {
-                JSONObject especieObj = especiesArray.getJSONObject(i);
-                for (String nombreEspecie : especieObj.keySet()) {
-                    JSONArray preguntas = especieObj.getJSONArray(nombreEspecie);
-                    if (preguntas.length() < 1) { // Reglas mínimas para diferenciación
-                        System.out.println("ERROR: La especie " + nombreEspecie + " no tiene preguntas suficientes para identificarla unívocamente.");
-                        return;
-                    }
-                    System.out.println("  Especie: " + nombreEspecie);
-                    for (int j = 0; j < preguntas.length(); j++) {
-                        JSONObject preguntaObj = preguntas.getJSONObject(j);
-                        for (String pregunta : preguntaObj.keySet()) {
-                            System.out.println("    - " + pregunta + " : " + preguntaObj.getBoolean(pregunta));
-                        }
-                    }
-                }
-            }
-        }
-    }
+    Gson gson = new Gson();
     
 }
