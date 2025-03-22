482f0af0d6ba0aa359cea661def512885dc9e31c
Miguelangel Maiz <miguelangel.maiz@correo.unimet.edu.ve>
2025-03-22 12:36:18 -0400

    alejandro modificacion

--- Proyecto_2_2425/src/EDD/Preguntas.java
+++ Proyecto_2_2425/src/EDD/Preguntas.java
@@ -8,6 +8,41 @@ package EDD;
  *
  * @author Miguel
  */
-public class preguntas {
+public class Preguntas {
+    private String pregunta;
+    private boolean respuesta;
+
+    public Preguntas(String pregunta, boolean respuesta) {
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+    }
+
+    /**
+     * @return the pregunta
+     */
+    public String getPregunta() {
+        return pregunta;
+    }
+
+    /**
+     * @param pregunta the pregunta to set
+     */
+    public void setPregunta(String pregunta) {
+        this.pregunta = pregunta;
+    }
+
+    /**
+     * @return the respuesta
+     */
+    public boolean isRespuesta() {
+        return respuesta;
+    }
+
+    /**
+     * @param respuesta the respuesta to set
+     */
+    public void setRespuesta(boolean respuesta) {
+        this.respuesta = respuesta;
+    }
     
 }
