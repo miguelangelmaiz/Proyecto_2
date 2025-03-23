c2493e99cfe835926b3baf31f8b4f48e16dcb665
chuchu1909 <131671310+chuchu1909@users.noreply.github.com>
2025-03-22 21:27:41 -0400

    Add files via upload

---
+++ Proyecto_2_2425/src/EDD/NodoAB.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package EDD;
+
+/**
+ *
+ * @author Miguel
+ */
+public class NodoAB {
+    
+    private String nombre;
+    private NodoAB si;
+    private NodoAB no;
+
+    public NodoAB(String nombre) {
+        this.nombre = nombre;
+        this.si = null;
+        this.no = null;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the si
+     */
+    public NodoAB getSi() {
+        return si;
+    }
+
+    /**
+     * @param si the si to set
+     */
+    public void setSi(NodoAB si) {
+        this.si = si;
+    }
+
+    /**
+     * @return the no
+     */
+    public NodoAB getNo() {
+        return no;
+    }
+
+    /**
+     * @param no the no to set
+     */
+    public void setNo(NodoAB no) {
+        this.no = no;
+    }
+}
+    
+    
+
---
+++ Proyecto_2_2425/src/proyecto_2_2425/Proyecto_2_2425.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_2_2425;
+
+/**
+ *
+ * @author Miguel
+ */
+public class Proyecto_2_2425 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
